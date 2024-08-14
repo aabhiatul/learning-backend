db8be3eb16cd4e9a896d6f81f35c16dbfb9ac7df
Aabhi_atul <aabhi_atul@Aabhi-atuls-MacBook-Air.local>
2024-08-14 22:07:43 +0530

    Manage errors and response in utils

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import express from "express";
 import connectDB from "./db/index.js";
-import { DB_NAME } from "./constants.js";
+const app = express();
 
 dotenv.config({
   path: "./.env",
 });
-console.log(process.env.MONGODB_URI, DB_NAME);
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("ERROR", (error) => {
+      console.log("Error", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at Port:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed!!!", error);
+  });
 // const app = express()(async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("ERROR", (error) => {
-//       console.log("Error", error);
-//       throw error;
-//     });
+// app.on("ERROR", (error) => {
+//   console.log("Error", error);
+//   throw error;
+// });
 //     app.listen(process.env.PORT, () => {
 //       console.log(`App is listening on port ${process.env.PORT}`);
 //     });
